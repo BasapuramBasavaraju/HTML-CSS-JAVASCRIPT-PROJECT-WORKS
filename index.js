f8f36bfd3b747000f61e4e835b154a17679004af
BasapuramBasavaraju <124877712+BasapuramBasavaraju@users.noreply.github.com>
2024-06-15 16:57:01 +0530

    Online Food Delivery

---
+++ index.js
@@ -0,0 +1,8 @@
+function showPopup1() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+    }
+function hidePopup1() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+    }
